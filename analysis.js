8f2957ff7ddb9e3d2a3da8990d826511383b2773
Alex <alexruan2001@gmail.com>
2018-03-20 18:31:45 -0400

    condensed analysis

--- analysis.js
+++ analysis.js
@@ -28,45 +28,33 @@ var app = new Vue({
             var stuff = {}
             matches = _.filter(this.rawData, ["team", Number(this.team)]) //finding all the matches that match the team number
             stuff.number = this.team //setting the number to be displayed in the card
-            stuff.drivetype = matches[0].drive + " " + matches[0].wheels //displaying the drivetype
+           // stuff.drivetype = matches[0].drive + " " + matches[0].wheels //displaying the drivetype
             stuff.matches = _.map(matches, "match"); //gets all of the match numbers
             stuff.fouls = _.map(matches, function (item) {
                 if (item.foul) return { c: "r", match: item.match }
                 else return { c: "g", match: item.match }
             });
             stuff.moved = _.map(matches, function (item) {
-                if (item.autonmovement != "moves") return { c: "r", match: item.match }
+                if (item.a_movement != "moves") return { c: "r", match: item.match }
                 else return { c: "g", match: item.match }
             });
             stuff.crossed = _.map(matches, function (item) {
-                if (item.autonmovement != "cross") return { c: "r", match: item.match }
+                if (item.a_movement != "cross") return { c: "r", match: item.match }
                 else return { c: "g", match: item.match }
             });
-            stuff.startgears = _.map(matches, function (item) {
-                if (!_.includes(item.autongears, "starts")) return { c: "r", match: item.match }
+            stuff.a_switch = _.map(matches, function (item) {
+                if (!_.includes(item.a_switch, "left" || "right")) return { c: "r", match: item.match }
                 else return { c: "g", match: item.match }
             });
-            stuff.autonswitch = _.map(matches, function (item) {
-                if (!_.includes(item.autonswitch, "left" || "right")) return { c: "r", match: item.match }
+            stuff.a_scale = _.map(matches, function (item) {
+                if (!_.includes(item.a_scale, "left" || "right")) return { c: "r", match: item.match }
                 else return { c: "g", match: item.match }
             });
-            stuff.autonswitch = _.map(matches, function (item) {
-                if (!_.includes(item.autonswitch, "yes" || "no")) return { c: "r", match: item.match }
+            stuff.a_vault = _.map(matches, function (item) {
+                if (!_.includes(item.a_vault, "Yes")) return { c: "r", match: item.match }
                 else return { c: "g", match: item.match }
             });
-            stuff.autonscale = _.map(matches, function (item) {
-                if (!_.includes(item.autonscale, "left" || "right")) return { c: "r", match: item.match }
-                else return { c: "g", match: item.match }
-            });
-            stuff.autonscalecorrectcolor = _.map(matches, function (item) {
-                if (!_.includes(item.autonscale, "yes" || "no")) return { c: "r", match: item.match }
-                else return { c: "g", match: item.match }
-            });
-            stuff.autonsvault = _.map(matches, function (item) {
-                if (!_.includes(item.autonvault, "Yes")) return { c: "r", match: item.match }
-                else return { c: "g", match: item.match }
-            });
-            stuff.autonquality = _.round(_.meanBy(matches, "autonplacequality"), 3)
+            stuff.autonquality = _.round(_.meanBy(matches, "a_place_quality"), 3)
             stuff.defensive = _.map(matches, function (item) {
                 if (!item.defense) return { c: "r", match: item.match }
                 else return { c: "g", match: item.match }
@@ -75,41 +63,39 @@ var app = new Vue({
                 if (!item.teloprobostuck) return { c: "r", match: item.match }
                 else return { c: "g", match: item.match }
             });
-            stuff.switchcubesplaced = _.map(matches, "switchcubesplaced")
-            stuff.switchcubesfailed = _.map(matches, "switchcubesfailed")
-            stuff.scalecubesplaced = _.map(matches, "scalecubesplaced")
-            stuff.scalecubesfailed = _.map(matches, "scalecubesfailed")
-            stuff.vaultcubesplaced = _.map(matches, "vaultcubes")
+            stuff.switch_cubes = _.map(matches, "switch_cubes")
+            stuff.scale_cubes = _.map(matches, "scale_cubes")
+            stuff.vault_cubes = _.map(matches, "vault_cubes")
             stuff.defensive = _.map(matches, function (item) {
                 if (item.defensive) return { c: "r", match: item.match }
                 else return { c: "g", match: item.match }
             });
-            stuff.defscore = _.round(_.meanBy(matches, "defscore"), 3)
-            stuff.defpickup = _.round(_.meanBy(matches, "defpickup"), 3)
-            stuff.foulnumber = _.round(_.meanBy(matches, "foulnumber"), 3)
+            stuff.def_score = _.round(_.meanBy(matches, "def_score"), 3)
+            stuff.def_pickup = _.round(_.meanBy(matches, "def_pickup"), 3)
+            stuff.foul_number = _.round(_.meanBy(matches, "foul_number"), 3)
             //stuff.cycletime = _.round(_.mean(_.compact(_.map(matches, "telopgearstime"))), 3)
             stuff.comments = {}
             stuff.comments.text = _.map(matches, "comments")
             stuff.comments.match = _.map(matches, "match")
             stuff.comments.names = _.map(matches, "name")
-            stuff.hanging = _.map(matches, function (item) {
+            stuff.hang = _.map(matches, function (item) {
                 if (item.hang == 'success') return { c: "g", match: item.match }
                 else return { c: "r", match: item.match }
             });
-            stuff.hangtime = _.round(_.mean(_.compact(_.map(matches, "hangtime"))), 3)
-            stuff.usedlevitate = _.map(matches, function (item) {
-                if (item.endlevitate != "Yes") return { c: "r", match: item.match }
+            stuff.hang_time = _.round(_.mean(_.compact(_.map(matches, "hang_time"))), 3)
+            stuff.levitate = _.map(matches, function (item) {
+                if (item.levitate != "Yes") return { c: "r", match: item.match }
                 else return { c: "g", match: item.match }
             });
-            stuff.hangsonother = _.map(matches, function (item) {
+            stuff.hanged_on_other = _.map(matches, function (item) {
                 if (item.hangsonother) return { c: "r", match: item.match }
                 else return { c: "g", match: item.match }
             });
-            stuff.otherhangson = _.map(matches, function (item) {
+            stuff.can_carry = _.map(matches, function (item) {
                 if (item.otherhangson) return { c: "r", match: item.match }
                 else return { c: "g", match: item.match }
             });
-            stuff.endrobothangtotal = _.map(matches, "endrobothangtotal")
+            stuff.robot_hang_total = _.map(matches, "endrobothangtotal")
             stuff.raw = matches;
             console.log(stuff)
             this.data.unshift(stuff)
