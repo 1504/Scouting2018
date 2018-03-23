6b9efbedc7dee25b066d99573b765f48b7a3b6bd
Sci-Phy <tristanlusardi@gmail.com>
2018-03-23 09:38:19 -0400

    Revert "Revert "removed error""
    This reverts commit 5b6d9260b23a48f05004216317b8404fff96de54.

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var app = new Vue({
             self.d = resp.body
             console.log(self.d)
         }, function() {
-            alert("An error occured, please refresh the page")
+        
         });
         this["default"] = JSON.parse(JSON.stringify(this.d));
         this.data = store('data') || [];
