4b93dd8b044c277da46cd1361741cec212846698
Ajusa <arhamjain@gmail.com>
2018-03-23 23:54:21 -0400

    really good start on data analysis
    hopefully we continue this!

--- analysis.js
+++ analysis.js
@@ -1,89 +1,62 @@
 var app = new Vue({
     el: '#app',
-    created: function () {
-        this.$http.get('/api').then(function (response) {
+    created: function() {
+        var self = this;
+        this.$http.get('/fields.json').then(function(resp) {
+            self.defaults = resp.body
+        });
+        this.$http.get('/api').then(function(response) {
             this.rawData = JSON.parse(response.body)
+            self.submit()
         });
         setInterval(this.update, 3000);
     },
     data: {
-        team: "",
+        team: "1504",
+        defaults: {},
         rawData: {},
         data: [],
-        ss: window.ss
+        s: window.ss,
+        _: _,
     },
     methods: {
-        update: function () {
-            this.$http.get('/api').then(function (response) {
+        update: function() {
+            this.$http.get('/api').then(function(response) {
                 this.rawData = JSON.parse(response.body)
             });
         },
-        openRaw: function (raw, team) {
+        openRaw: function(raw, team) {
             //console.log(raw)
             swal({ title: team, html: "<pre>" + JSON.stringify(raw) + "</pre>" })
         },
-        submit: function () {
+        submit: function() {
             //generate statistics, with actual comments because I am a good programmer.
             //I also wont understand this without comments
-            var stuff = {}
+            var team = {}
             matches = _.filter(this.rawData, ["team", Number(this.team)]) //finding all the matches that match the team number
-            stuff.number = this.team //setting the number to be displayed in the card
-           // stuff.drivetype = matches[0].drive + " " + matches[0].wheels //displaying the drivetype
-            stuff.matches = _.map(matches, "match"); //gets all of the match numbers
-            stuff.fouls = _.map(matches, function (item) {
-                if (item.foul) return { c: "r", match: item.match }
-                else return { c: "g", match: item.match }
-            });
-            stuff.moved = _.map(matches, function (item) {
-                if (item.a_movement != "moves") return { c: "r", match: item.match }
-                else return { c: "g", match: item.match }
-            });
-            stuff.crossed = _.map(matches, function (item) {
-                if (item.a_movement != "cross") return { c: "r", match: item.match }
-                else return { c: "g", match: item.match }
-            });
-            stuff.a_switch_cubes = _.map(matches, "a_switch_cubes")
-            stuff.a_scale_cubes = _.map(matches, "a_scale_cubes")
-            stuff.a_vault_cubes = _.map(matches, "a_vault_cubes")
-            stuff.autonquality = _.round(_.meanBy(matches, "a_place_quality"), 3)
-            stuff.defensive = _.map(matches, function (item) {
-                if (!item.defense) return { c: "r", match: item.match }
-                else return { c: "g", match: item.match }
-            });
-            stuff.stuck = _.map(matches, function (item) {
-                if (!item.teloprobostuck) return { c: "r", match: item.match }
-                else return { c: "g", match: item.match }
-            });
-            stuff.switch_cubes = _.map(matches, "switch_cubes")
-            stuff.scale_cubes = _.map(matches, "scale_cubes")
-            stuff.vault_cubes = _.map(matches, "vault_cubes")
-            stuff.switch_cubes_failed = _.map(matches, "switch_cubes_failed")
-            stuff.scale_cubes_failed = _.map(matches, "scale_cubes_failed")
-            stuff.defensive = _.map(matches, function (item) {
-                if (item.defensive) return { c: "r", match: item.match }
-                else return { c: "g", match: item.match }
-            });
-            stuff.def_cubes = _.round(_.meanBy(matches, "def_cubes"), 3)
-            stuff.foul_number = _.round(_.meanBy(matches, "foul_number"), 3)
-            //stuff.cycletime = _.round(_.mean(_.compact(_.map(matches, "telopgearstime"))), 3)
-            stuff.comments = {}
-            stuff.comments.text = _.map(matches, "comments")
-            stuff.comments.match = _.map(matches, "match")
-            stuff.comments.names = _.map(matches, "name")
-            stuff.hang = _.map(matches, function (item) {
-                if (item.hang == 'success') return { c: "g", match: item.match }
-                else return { c: "r", match: item.match }
-            });
-            stuff.time_to_hang = _.round(_.mean(_.compact(_.map(matches, "time_to_hang"))), 3)
-            stuff.hanged_on_other = _.map(matches, function (item) {
-                if (item.hangsonother) return { c: "r", match: item.match }
-                else return { c: "g", match: item.match }
-            });
-            stuff.carry_number = _.map(matches, "carry_number")
-            stuff.raw = matches;
-            console.log(stuff)
-            this.data.unshift(stuff)
-        }
+            matches = _.sortBy(_.uniqBy(matches, 'match'), "match"); //make sure only one match per robot, and sort it
+            for (var i = matches.length - 1; i >= 0; i--) {
+                var obj = JSON.parse(JSON.stringify(this.defaults));
+                for (var key in matches[i]) {
+                    if (matches[i].hasOwnProperty(key)) {
+                        obj[key] = matches[i][key]
+                    }
+                }
+                matches[i] = obj;
+                for (var key in matches[i]) {
+                    if (matches[i].hasOwnProperty(key)) {
+                        (team[key] = team[key] || []).unshift(matches[i][key]);
+                    }
+                }
+            }
+            this.team = ""
+            //at this point, we have a team object with all of the data about a team, ordered by match number
+            this.data.unshift(this.analyze(team))
+        },
+        analyze: function(team){
+            console.log(team)
+            return team;
+        },
     },
     components: {
         'matches': {
